92383ba49d0166ea877afea38cd234624786b3fa
nandoripardo888 <nando.lfrs84@gmail.com>
2024-01-13 19:01:17 -0400

    incremento classes css

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Square from "./Square";
+import { List } from "postcss/lib/list";
 
 
 const Board = () => {
@@ -15,31 +16,55 @@ const Board = () => {
         setXIsNext(!xIsNext);
     }
 
-    const renderSquare = (i:number) => {
+    const calculateWinner = (squares:Array<number>) => {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ]
+        for (let i=0; i<lines.length; i++){
+            const [a,b,c] = lines[i];
+            if(squares[a] && squares[a] === squares[b] && squares[b] === squares[c] &&squares[c]){
+                return squares[a]
+            }
+        }
+        return null;
+    }
+
+    const renderSquare = (i:number, quina:number) => {
         return(
-            <Square value={squares[i]} onClick={()=>handleClick(i)} />
+            <Square value={squares[i]} onClick={()=>handleClick(i)} quina={quina}/>
         )
     }
 
+    const winner = calculateWinner(squares);
+    const status = winner ? "Winner: " + winner : 'Next player: ' + (xIsNext ? 'X':'O');
+    //const status = winner ? "Winner: " + winner : 'Next player: ' + (xIsNext ? 'X' : 'O');
+
     return (
-        <div>
-            <div style={{marginBottom:"10px"}}>
-                Status do jogo...
+        <div className="board">
+            <div className="status">
+                {status}
             </div>
             <div className="board-row">
-                {renderSquare(0)}
-                {renderSquare(1)}
-                {renderSquare(2)}
+                {renderSquare(0,1)}
+                {renderSquare(1,0)}
+                {renderSquare(2,2)}
             </div>
             <div className="board-row">
-                {renderSquare(3)}
-                {renderSquare(4)}
-                {renderSquare(5)}
+                {renderSquare(3,0)}
+                {renderSquare(4,0)}
+                {renderSquare(5,0)}
             </div>
             <div className="board-row">
-                {renderSquare(6)}
-                {renderSquare(7)}
-                {renderSquare(8)}
+                {renderSquare(6,3)}
+                {renderSquare(7,0)}
+                {renderSquare(8,4)}
             </div>
         </div>
     )
--- components/Square.tsx
+++ components/Square.tsx
@@ -3,11 +3,20 @@ import React from 'react';
 interface SquareProps {
   value: string;
   onClick: () => void;
+  quina: number;
 }
 
 const Square = (props:SquareProps) => {
+
+  const styleQuina = {
+    borderTopLeftRadius: props.quina === 1 ? "10%" : "0%",
+    borderTopRightRadius:props.quina === 2 ? "10%" : "0%",
+    borderBottomLeftRadius:props.quina === 3 ? "10%" : "0%",
+    borderBottomRightRadius:props.quina === 4 ? "10%" : "0%",
+  }
+
   return (
-    <button className="square" onClick={props.onClick}>
+    <button className="square" onClick={props.onClick} style={styleQuina}>
       {props.value}
     </button>
   );
